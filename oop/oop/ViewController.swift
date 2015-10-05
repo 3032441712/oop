463ed9f6dd8f8cd3897b9cf289206e46243c44ca
3032441712 <3032441712@qq.com>
2015-10-05 15:19:43 +0800

    init code

---
+++ oop/oop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  oop
+//
+//  Created by 赵严 on 15/10/5.
+//  Copyright © 2015年 赵严. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
