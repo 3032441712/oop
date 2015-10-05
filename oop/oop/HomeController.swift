cfbf6160e9fd48ca2f87b590dec3c90ad8c17888
3032441712 <3032441712@qq.com>
2015-10-05 15:28:44 +0800

    TODO:初始化代码目录结构. 1 初始化代码目录结构. 2 初始化首页代码.

--- oop/oop/HomeController.swift
+++ oop/oop/HomeController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("viewDidLoad");
     }
 
     override func didReceiveMemoryWarning() {
